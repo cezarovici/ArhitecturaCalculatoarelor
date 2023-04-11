d7af3ebe0d853186ea8783e88dc96970c76ae046
Apetroaei Cezar <njroffic@gmail.com>
2023-04-11 15:54:45 +0300

    ab 8 init

--- Labolator7/Labolator7.srcs/sim_1/new/test_tb.v
+++ Labolator7/Labolator7.srcs/sim_1/new/test_tb.v
@@ -3,47 +3,76 @@ module tb_top(
 );
     reg clk;
     reg res;
-    reg [6:0] opcode;
+    reg PCWriteCond;
+    reg PCWrite;
+    reg IorD;
+    reg  MemRead;
+    reg MemWrite;
+    reg IRWrite;
+    reg RegDst;
+    reg RegWrite;
+    reg ALUSrcA;
+    reg [1:0]ALUSrcB;
+    reg  [1:0]ALUOp;
+    reg  PCSource;
     
-    wire PCWriteCond;
-    wire PCWrite;
-    wire IorD;
-    wire MemRead;
-    wire MemWrite;
-    wire MemtoReg;
-    wire IRWrite;
-    wire RegWrite;
-    wire ALUSrcA;
-    wire [1:0] ALUSrcB;
-    wire [1:0] ALUOp;
-    wire PCSource;
+    wire [6:0]op_code;
+   
+   data_path DUT( clk,res,
 
-control_path DUT(
-    .clk(clk),
-    .res(res),
-    .PCWriteCond(PCWriteCond),
-    .PCWrite(PCWrite),
-    .IorD(IorD),
-    .MemRead(MemRead),
-    .MemWrite(MemWrite),
-    .MemtoReg(MemtoReg),
-    .IRWrite(IRWrite),
-    .RegWrite(RegWrite),
-    .ALUSrcA(ALUSrcA),
-    .ALUSrcB(AluSrcB),
-    .ALUOp(ALUOp),
-    .PCSource(PCSource)
+    PCWriteCond,
+    PCWrite,
+    IorD,
+    MemRead,
+    MemWrite,
+    IRWrite,
+    RegDst,
+    RegWrite,
+    ALUSrcA,
+    ALUSrcB,
+     ALUOp,
+     PCSource,
     
-);
+    op_code);
+    
+   initial begin
+   
+   clk=0;
+   res=0;
 
-initial begin
-clk = 0; res = 0; 
-opcode = 7'b0000011;
-#10 res = 1;
-#10 res = 0;
-#200 $finish;
+    PCWriteCond=0;
+    PCWrite=0;
+    IorD=0;
+    MemRead=0;
+    MemWrite=0;
+    IRWrite=0;
+    RegDst=0;
+    RegWrite=0;
+    ALUSrcA=0;
+    ALUSrcB=0;
+    ALUOp=0;
+    PCSource=0;
+    
+    
+     #10 res =1;
+     #10 res =0;
+     
+     MemRead = 1;
+     IRWrite = 1;
+     ALUSrcB = 2'b01;
+     PCWrite = 1;
+     
+     #10
+      MemRead = 0;
+     IRWrite = 0;
+     ALUSrcB = 2'b00;
+     PCWrite = 0;
+     
+     
+     #100 $finish;
+
 end
 
-always #5 clk = ~clk;
+always #5 clk=~clk;
 
 endmodule
--- Labolator7/Labolator7.srcs/sources_1/new/main.v
+++ Labolator7/Labolator7.srcs/sources_1/new/main.v
@@ -78,3 +78,195 @@ end
 end
      
 endmodule
+
+module data_path(
+	input clk,
+	input res,
+	
+	input PCWriteCond,
+	input PCWrite,
+	input IorD,
+	input MemRead,
+	input MemWrite,
+	input IRWrite,
+	input RegDst,
+	input RegWrite, 
+	input ALUSrcA, 
+	input [1:0] ALUSrcB, 
+	input [1:0] ALUOp, 
+	input PCSource,
+	
+	output [6:0] op_code
+);
+
+reg [31:0] PC;
+reg [31:0] IR;
+
+reg [31:0] ALUout;
+
+reg [31:0] A;
+
+reg [31:0] B;
+
+reg [31:0] MDR;
+
+reg [7:0] mem [0:1023];
+
+reg [31:0] regs [0:31];
+
+wire [31:0] addr_mem;
+
+wire Zero;
+
+wire [31:0] da;
+
+wire [31:0] db;
+
+reg [31:0] alu;
+
+wire [31:0] opA;
+
+wire [31:0] opB;
+
+wire [4:0] rc;
+
+reg [31:0] imm32;
+
+assign Zero = (alu == 0) ? 1 : 0;
+
+assign opA = (ALUSrcA == 1) ? A : PC;
+
+assign opB = (ALUSrcB == 2'b00) ? 	B :
+			 ((ALUSrcB == 2'b01) ? 	4 :
+			 ((ALUSrcB == 2'b10) ? 	imm32 : 0 ));
+
+// imm32 model
+always@(IR) begin
+	        case(IR[6:0])
+            7'b0000011,
+            7'b0001111,
+            7'b0011011,
+            7'b1100111,
+            7'b1110011,
+            7'b0010011: imm32 = { {20{IR[31]}}, IR[31:20]};
+            7'b0100011: imm32 = { {20{IR[31]}}, IR[31:25], IR[11:7]};
+            7'b1100011: imm32 = { {20{IR[31]}}, IR[7], IR[30:25], IR[11:8], 1'b0};            
+            7'b1101111: imm32 = { {12{IR[31]}}, IR[19:12], IR[20], IR[30:25], IR[11:8], 1'b0};            
+            7'b0010111,
+            7'b0110111: imm32 = { IR[31:12], {12{1'b0}}}; 
+            default:
+                imm32 = 32'h0000_0000;
+        endcase
+end
+
+// ALU model
+always@(ALUOp or opA or opB or IR)
+	casex({ALUOp, IR[31:25], IR[14:12]})
+	
+		// TBD
+		12'b00_xxxxxxx_xxx,
+		12'b10_0000000_000,
+		12'b11_xxxxxxx_000: alu = opA + opB;
+		
+		12'b10_0100000_000,
+		12'b01_xxxxxxx_xxx: alu = opA - opB;
+		
+		12'b10_0000000_111,
+		12'b11_xxxxxxx_111: alu = opA & opB;
+		
+		12'b10_0000000_110,
+		12'b11_xxxxxxx_110: alu = opA | opB;
+		
+		12'b10_0000000_100,
+		12'b11_xxxxxxx_100: alu = opA ^ opB;
+		
+		default: alu = 32'b0; // !!! 
+	endcase
+
+// PC model
+always@(posedge clk)
+	if (res == 1) 
+		PC <= 0;
+	else
+		casex({PCWrite, PCWriteCond, Zero, PCSource})
+			4'b1_x_x_0: PC <= alu;
+			4'b1_x_x_1: PC <= ALUout;
+			4'b0_1_1_x: PC <= ALUout;
+			
+			default: PC <= PC;
+		endcase
+
+// IR model
+always@(posedge clk)
+	casex({res, IRWrite})
+		2'b1_x : IR <= 0;
+		2'b0_1 : begin 
+			IR[31:24] 	<= mem[addr_mem+3];
+			IR[23:16] 	<= mem[addr_mem+2];
+			IR[15:8] 	<= mem[addr_mem+1];
+			IR[7:0] 	<= mem[addr_mem];
+			end
+	endcase
+
+assign addr_mem = (IorD == 0) ? PC : ALUout;
+
+always@(posedge clk)
+	ALUout <= alu;
+
+always@(posedge clk)
+	A <= da;
+
+always@(posedge clk)
+	B <= db;
+
+always@(posedge clk) begin
+	MDR[31:24] 	<= mem[addr_mem+3];
+	MDR[23:16] 	<= mem[addr_mem+2];
+	MDR[15:8] 	<= mem[addr_mem+1];
+	MDR[7:0] 	<= mem[addr_mem];	
+end
+
+wire [4:0] ra = IR[ 19:15 ];
+
+assign da = regs[ra];
+
+wire [4:0] rb = IR[ 24:20];
+
+assign db = regs[rb];	
+
+assign rc = IR[ 11:7 ];
+
+// Register file model
+always@(posedge clk)
+	if (res == 1) begin
+		regs[0] <= 0; regs[1] <= 0; regs[2] <= 0; regs[3] <= 0; regs[4] <= 0; regs[5] <= 0;
+		regs[6] <= 0; regs[7] <= 0; regs[8] <= 0; regs[9] <= 0; regs[10] <= 0; regs[11] <= 0;
+		regs[12] <= 0; regs[13] <= 0; regs[14] <= 0; regs[15] <= 0; regs[16] <= 0; regs[17] <= 0;
+		regs[18] <= 0; regs[19] <= 0; regs[20] <= 0; regs[21] <= 0; regs[22] <= 0; regs[23] <= 0;
+		regs[24] <= 0; regs[25] <= 0; regs[26] <= 0; regs[27] <= 0; regs[28] <= 0; regs[29] <= 0;
+		regs[30] <= 0; regs[31] <= 0;
+	end else if (rc != 0 && RegWrite == 1) 
+		regs[rc] <= MDR; 
+
+// memory write operation
+always@(posedge clk)
+	if (MemWrite == 1) begin
+		mem[addr_mem+3]	<= B[31:24];
+		mem[addr_mem+2] <= B[23:16];
+		mem[addr_mem+1] <= B[15:8];
+		mem[addr_mem+0] <= B[7:0];	
+	end
+	
+assign op_code = IR[6:0];
+	
+initial begin
+	// mem init with some code and data
+	// codul incepe la adresa 0 
+	
+	mem[3] = 8'h00; mem[2] = 8'h53; mem[1] = 8'h03; mem[0] = 8'hb3;  
+	
+	// datele sunt stocate de la adresa 256
+	mem[259] = 8'h0; mem[258] = 8'h0; mem[257] = 8'b0; mem[256] = 8'h0;
+end
+
+endmodule
